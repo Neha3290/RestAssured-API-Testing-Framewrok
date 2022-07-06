1bb2d9aa3be3a32bf21818e46c055b3e5cb7808e
neha3290 <nehagupta3290@gmail.com>
2022-07-06 13:28:32 +0530

    Initial commit

---
+++ src/test/java/com/restAssured/spotify/application/SpecBuilder.java
@@ -0,0 +1,34 @@
+package com.restAssured.spotify.application;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class SpecBuilder {
+
+    public static String access_token="BQCUglAskQuj-ofyBNijkkncAbSo9Of7dAIvNeh_-3lBsd-_zR4ZnyV-o7o_TbglclpjfJ9xaLWfZy-NL1N5XfVsdyra2f4du55ChzZbH5575oRAfYYO-UHipqQI7Qi2_kgwxM_nxeD_Qlty0EiXZiyfdsFfi0Tapb9I_C_uLThQh_kBITzfDtTeUCxApdXbMJa5O2FpujaukdIb8bK8PqomdNgBZCPYVgt1bFwljkZxUa_LIyX8wHrUh5_MpOvHspJDL104NdUkzYXg";
+
+
+    public static RequestSpecification getRequestSpec(){
+
+        return new RequestSpecBuilder().
+                setBaseUri("https://api.spotify.com/").
+                setBasePath("v1/").
+                addHeader("Authorization", "Bearer "+access_token).
+                setContentType(ContentType.JSON).
+                log(LogDetail.ALL).
+                build();
+
+    }
+
+    public static ResponseSpecification getResponseSpec(){
+        return new ResponseSpecBuilder().
+                log(LogDetail.ALL).build();
+
+    }
+
+
+}
---
+++ src/test/java/com/restAssured/spotify/tests/Playlist.java
@@ -0,0 +1,129 @@
+package com.restAssured.spotify.tests;
+
+
+import com.restAssured.spotify.pojo.ErrorPojo;
+import com.restAssured.spotify.pojo.PlaylistPojo;
+import org.testng.annotations.Test;
+
+import static com.restAssured.spotify.application.SpecBuilder.getRequestSpec;
+import static com.restAssured.spotify.application.SpecBuilder.getResponseSpec;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class Playlist {
+
+    @Test
+    public void shouldBeCreateANewPlaylist(){
+
+        PlaylistPojo requestPlaylist = new PlaylistPojo();
+        requestPlaylist.setName("RestAssured Playlist1");
+        requestPlaylist.setDescription("New playlist description1");
+        requestPlaylist.setPublic(false);
+
+        PlaylistPojo responsePlaylist = given(getRequestSpec()).
+                body(requestPlaylist).
+        when().
+                post("users/31cqdzauwxebovgzf2karjxnsbxq/playlists").
+        then().spec(getResponseSpec()).
+                assertThat().
+                statusCode(201).
+                extract().
+                response().
+                as(PlaylistPojo.class);
+
+        assertThat(responsePlaylist.getName(), equalTo(requestPlaylist.getName()));
+        assertThat(responsePlaylist.getDescription(), equalTo(requestPlaylist.getDescription()));
+        assertThat(responsePlaylist.getPublic(), equalTo(requestPlaylist.getPublic()));
+
+    }
+
+    @Test
+    public void shouldGetAPlaylist(){
+
+        PlaylistPojo responsePlaylist = given(getRequestSpec()).
+        when().
+                get("playlists/3tcYXCP07i4U4WcFH3KQ9h").
+        then().spec(getResponseSpec()).
+                assertThat().
+                statusCode(200).
+                extract().
+                response().
+                as(PlaylistPojo.class);
+        assertThat(responsePlaylist.getName(), equalTo("Updated Playlist Name"));
+        assertThat(responsePlaylist.getDescription(), equalTo("Updated playlist description"));
+        assertThat(responsePlaylist.getPublic(), equalTo(false));
+
+    }
+
+
+    @Test
+    public void shouldUpdateThePlaylist(){
+        PlaylistPojo requestPlaylist = new PlaylistPojo();
+        requestPlaylist.setName("Updated RestAssured Playlist1");
+        requestPlaylist.setDescription("Updated New playlist description1");
+        requestPlaylist.setPublic(false);
+
+        given(getRequestSpec()).
+                body(requestPlaylist).
+        when().
+                put("playlists/4sq0QAmRjSMBDSAP1tJaox").
+        then().spec(getResponseSpec()).
+                assertThat().
+                statusCode(200);
+
+    }
+
+
+    @Test
+    public void shouldNotCreateAPlaylistWithoutName(){
+        PlaylistPojo requestPlaylist = new PlaylistPojo();
+        requestPlaylist.setName("");
+        requestPlaylist.setDescription("Updated New playlist description1");
+        requestPlaylist.setPublic(false);
+
+       ErrorPojo responsePlaylist = given(getRequestSpec()).
+                body(requestPlaylist).
+        when().
+                post("users/31cqdzauwxebovgzf2karjxnsbxq/playlists").
+        then().spec(getResponseSpec()).
+                assertThat().
+                statusCode(400).
+               extract().
+               response().
+               as(ErrorPojo.class);
+
+       assertThat(responsePlaylist.getError().getStatus(),equalTo(400));
+       assertThat(responsePlaylist.getError().getMessage(),equalTo("Missing required field: name"));
+
+    }
+
+
+    @Test
+    public void shouldNotCreateAPlaylistWitExpiredToken(){
+        PlaylistPojo requestPlaylist = new PlaylistPojo();
+        requestPlaylist.setName("Expired Token Playlist");
+        requestPlaylist.setDescription("Updated New playlist description1");
+        requestPlaylist.setPublic(false);
+
+        ErrorPojo responsePlaylist = given().
+                baseUri("https://api.spotify.com/v1/").
+                header("Authorization","Bearer BQDzq6Nhg71OIEOm5jqm4AVpEKLd-lHOEkePH0ydUckVKVmkA1MngmjDSwtRoA7TtUd2LW3ThPg1t1Ne-QklTpXqh7F-0CUBXHZ95GVt41e5CGYpqkjrpMOBKxey_O2sBD8mQOfuQu8u7iFvr8grkL841J92ERa28fC6lkXrlAX8ouAgr9IIuWQNpv9EMhYdloUHhiwisD7hsh_yMhFz5_J31YUDP5XJ_gmm-qNX61Y6aQFTsuwfBNPca15l6JaSbR99T1v_6aD1lRZE").
+                header("Content-Type","application/json").
+                body(requestPlaylist).
+                log().all().
+                when().
+                post("users/31cqdzauwxebovgzf2karjxnsbxq/playlists").
+                then().spec(getResponseSpec()).
+                extract().
+                response().
+                as(ErrorPojo.class);
+        assertThat(responsePlaylist.getError().getStatus(),equalTo(401));
+        assertThat(responsePlaylist.getError().getMessage(),equalTo("The access token expired"));
+
+    }
+
+
+
+}
